be5a4c68862911591b88b3005c0dc29288f506b5
jirayaW <jiraya.pla@gmail.com>
2024-07-12 10:13:07 +0700

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { useEffect } from 'react';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -15,6 +16,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then(registration => {
+            console.log('SW registered: ', registration);
+          })
+          .catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+          });
+      });
+    }
+  }, []);
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
